d4856d5501e8de40b152449b79c80ab1017ca7aa
Diego <blasquez.diego@gmail.com>
2019-05-24 23:13:49 -0500

    Commit Direccion Server

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import { UsuarioModelo } from './../modelos/usuario-model';
 import { HttpHeaders } from '@angular/common/http';
 
-export const API_URL: String = "http://ec2-18-222-223-75.us-east-2.compute.amazonaws.com:6002/api/usuario";
+export const API_URL: String = "http://ec2-13-58-96-6.us-east-2.compute.amazonaws.com:6002/api/usuario";
 //export const API_URL: String = "http://192.168.1.70:6002/api/usuario";
 export const HTTPOptions = {
     headers: new HttpHeaders({
